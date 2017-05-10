856ab811e3937f6aaa54be08a88eebbc08a91a99
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-10 17:28:00 -0500

    Missing brackets

--- install_cli.sh
+++ install_cli.sh
@@ -1,8 +1,11 @@
 # Checking if bx is installed
+grn=$'\e[1;32m'
+end=$'\e[0m'
+
 BX_PATH=$(command -v bx)
 
 if [[ $? -ne 0 ]]; then
-	printf "\n\nInstalling Bluemix CLI (bx)\n"
+	printf "\n\n${grn}Installing Bluemix CLI (bx)...${end}\n"
 
 	if [[ $OSTYPE =~ .*darwin.* ]]; then
 		curl -o Bluemix_CLI_0.5.2.pkg http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_0.5.2.pkg
@@ -24,7 +27,7 @@ bx update &> /dev/null
 # Check if bx cs is installed
 bx cs &> /dev/null
 if [[ $? -ne 0 ]]; then
-	printf "\n\nInstalling Bluemix Container Service (bx cs) plugin...\n"
+	printf "\n\n${grn}Installing Bluemix Container Service (bx cs) plugin...${end}\n"
 	bx plugin install container-service -r Bluemix
 fi
 
@@ -32,7 +35,7 @@ fi
 KUBE_PATH=$(command -v kubectl)
 
 if [[ $? -ne 0 ]]; then
-	printf "\n\nInstalling Kubernetes CLI (kubectl)\n"
+	printf "\n\n${grn}Installing Kubernetes CLI (kubectl)...${end}\n"
 
 	if [[ $OSTYPE =~ .*darwin.* ]]; then
 		# OS X
@@ -52,7 +55,7 @@ fi
 KUBE_PATH=$(command -v helm)
 
 if [[ $? -ne 0 ]]; then
-	printf "\n\nInstalling Helm CLI (helm)\n"
+	printf "\n\n${grn}Installing Helm CLI (helm)...${end}\n"
 
 	curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 	chmod 700 get_helm.sh
@@ -65,13 +68,13 @@ fi
 JQ_PATH=$(command -v jq)
 
 if [[ $? -ne 0 ]]; then
-	printf "\n\nInstalling jq\n"
+	printf "\n\n${grn}Installing jq${end}\n"
 
 	if [[ $OSTYPE =~ .*darwin.* ]]; then
 		# OS X
 		brew install jq
 
-	elif [ $OSTYPE =~ .*linux.* ]]; then
+	elif [[ $OSTYPE =~ .*linux.* ]]; then
 		# Linux
 		sudo apt-get install jq
 	fi
