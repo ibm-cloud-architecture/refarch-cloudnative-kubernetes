3d429d778aaa24cd6d0ff8026a6c956b34dacef5
Fabio Gomez <fabiogomez@us.ibm.com>
2017-05-10 16:21:11 -0500

    - Updated README - Updated script that installs bluecompute

--- install_bluecompute.sh
+++ install_bluecompute.sh
@@ -8,9 +8,13 @@ blu=$'\e[1;34m'
 mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
+coffee=$'\xE2\x98\x95'
+coffee3="${coffee} ${coffee} ${coffee}"
 
 API="api.ng.bluemix.net"
 CLUSTER_NAME=$1
+SPACE=$2
+API_KEY=$3
 
 function check_tiller {
 	kubectl --namespace=kube-system get pods | grep tiller | grep Runnin
@@ -18,18 +22,38 @@ function check_tiller {
 
 # Bluemix Login
 printf "${grn}Login into Bluemix${end}\n"
-bx login
+if [[ -z "${API_KEY// }" && -z "${SPACE// }" ]]; then
+	echo "${yel}API Key & SPACE NOT provided.${end}"
+	bx login -a ${API}
+
+elif [[ -z "${SPACE// }" ]]; then
+	echo "${yel}API Key provided but SPACE was NOT provided.${end}"
+	export BLUEMIX_API_KEY=${API_KEY}
+	bx login -a ${API}
+
+elif [[ -z "${API_KEY// }" ]]; then
+	echo "${yel}API Key NOT provided but SPACE was provided.${end}"
+	bx login -a ${API} -s ${SPACE}
+
+else
+	echo "${yel}API Key and SPACE provided.${end}"
+	export BLUEMIX_API_KEY=${API_KEY}
+	bx login -a ${API} -s ${SPACE}
+fi
 
 printf "\n\n${grn}Getting Account Information...${end}\n"
-SPACE=$(cat ~/.bluemix/.cf/config.json | jq .OrganizationFields.Name | sed 's/"//g')
-ORG=$(cat ~/.bluemix/.cf/config.json | jq .SpaceFields.Name | sed 's/"//g')
+ORG=$(cat ~/.bluemix/.cf/config.json | jq .OrganizationFields.Name | sed 's/"//g')
+SPACE=$(cat ~/.bluemix/.cf/config.json | jq .SpaceFields.Name | sed 's/"//g')
 
 # Creating for API KEY
-printf "\n\n${grn}Creating API KEY...${end}\n"
-API_KEY=$(bx iam api-key-create kubekey | tail -1 | awk '{print $3}')
-echo "API key 'kubekey' was created"
-echo "Please preserve the API key! It cannot be retrieved after it's created."
-echo "API Key = ${API_KEY}"
+if [[ -z "${API_KEY// }" ]]; then
+	printf "\n\n${grn}Creating API KEY...${end}\n"
+	API_KEY=$(bx iam api-key-create kubekey | tail -1 | awk '{print $3}')
+	echo "${yel}API key 'kubekey' was created.${end}"
+	echo "${mag}Please preserve the API key! It cannot be retrieved after it's created.${end}"
+	echo "${cyn}Name${end}	kubekey"
+	echo "${cyn}API Key${end}	${API_KEY}"
+fi
 
 printf "\n\n${grn}Login into Container Service${end}\n\n"
 bx cs init
@@ -40,7 +64,7 @@ if [[ -z "${CLUSTER_NAME// }" ]]; then
 
 	if [[ "$CLUSTER_NAME" == "Name" ]]; then
 		echo "No Kubernetes Clusters exist in your account. Please provision one and then run this script again."
-		return 1
+		exit 1
 	fi
 fi
 
@@ -52,7 +76,7 @@ echo "KUBECONFIG is set to = $KUBECONFIG"
 
 if [[ -z "${KUBECONFIG// }" ]]; then
 	echo "KUBECONFIG was not properly set. Exiting"
-	return 1
+	exit 1
 fi
 
 printf "\n\n${grn}Initializing Helm.${end}\n"
@@ -65,11 +89,12 @@ while [[ "${TILLER_DEPLOYED}" == "" ]]; do
 	TILLER_DEPLOYED=$(check_tiller)
 done
 
-printf "\n\n${grn}Installing BLUECOMPUTE Application. This will take a few minutes...${end}\n\n"
+printf "\n\n${grn}Pulling Bluecompute Chart dependencies...${end}\n\n"
 cd bluecompute
 helm repo add bc https://fabiogomezdiaz.github.io/refarch-cloudnative-kubernetes/charts
 helm dependency update
 
+printf "\n\n${grn}Installing BLUECOMPUTE Application. This will take a few minutes...${end} ${coffee3}\n\n"
 time helm install \
 --set configMap.bluemixOrg=${ORG} \
 --set configMap.bluemixSpace=${SPACE} \
@@ -83,5 +108,9 @@ kubectl delete jobs,pods -l heritage=Tiller
 
 cd ..
 
-# Provide links to bluecompute web app
-# Provide links to services in kube console
+printf "\n\n${grn}To see Kubernetes Dashboard, paste the following in your terminal:${end}\n"
+echo "export KUBECONFIG=${KUBECONFIG}"
+printf "\n${grn}Then run this command to connect to Kubernetes Dashboard:${end}\n"
+echo "kubectl proxy"
+printf "\n${grn}Finally, open a browser window and paste the following URL to see the Services created by Bluecompute Chart:${end}\n"
+echo "http://127.0.0.1:8001/api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard/#/service?namespace=default"
