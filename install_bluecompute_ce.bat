be1cb97b7db8f52c43a550b86727c6df266e30b0
RobertJBarron <robert.j.barron@gmail.com>
2017-06-21 15:57:20 +0300

    Add files via upload

--- install_bluecompute_ce.bat
+++ install_bluecompute_ce.bat
@@ -19,7 +19,7 @@ for /F %%c in ('echo %%alfanum:~!rnd_num!^,1%%') do set HS_256_KEY=!HS_256_KEY!%
 )
 
 rem echo HS_256_KEY=%HS_256_KEY%
-@echo on
+rem @echo on
 
 if  "%BX_REGION%" == "" (
    set BX_API_ENDPOINT=api.ng.bluemix.net
@@ -222,51 +222,80 @@ if %errorlevel% NEQ 0 (
    echo Could not install grafana. Exiting.
    exit /b 1
 )
-echo grafana-ce was successfully installed!
-
-echo Cleaning up...
-kubectl --namespace %NAMESPACE% delete pods,jobs -l heritage=Tiller
+echo grafana was successfully installed!
 
 :all_deployed
-echo Getting the correct WebPort
 
+:audit
+call audit_ce_master_install.bat
 
-:webport_loop_start
-    rem for /f %%i in ('kubectl get service bluecompute-web -o json | jq .spec.ports[0].nodePort') do @set WEBPORT=%%i
-    kubectl get service bluecompute-web -o json | for /f %%i in ('jq .spec.ports[0].nodePort') do @echo %%i > %TMP%\BC_Webport.tmp
-    for /f %%i in (%TMP%\BC_Webport.tmp) do @set WEBPORT=%%i
-
-    if "%WEBPORT%" == "null" (
-       timeout /t 1
-       goto :webport_loop_start
-    )   
-    if "%WEBPORT%" == "" (
-       timeout /t 1
-       goto :webport_loop_start
-    )   
-    goto webport_loop_exit
-goto :webport_loop_start
-:webport_loop_exit
-
+echo Getting the Node IP
 :getNodeIP
 kubectl get nodes -o jsonpath={.items[*].status.addresses[?(@.type==\"ExternalIP\")].address} > %TMP%\BC_NodeIP.tmp
 for /f %%i in (%TMP%\BC_NodeIP.tmp) do @set NODEIP=%%i
 
-:audit
-call audit_ce_master_install.bat
+
+echo Getting the correct WebPorts
+:bcwebport_loop_start
+    echo polling the service bluecompute-web in namespace %NAMESPACE% to get the webport
+    kubectl get service --namespace=%NAMESPACE% bluecompute-web -o json | for /f %%i in ('jq .spec.ports[0].nodePort') do @echo %%i > %TMP%\BC_Webport.tmp
+    for /f %%i in (%TMP%\BC_Webport.tmp) do @set BCWEBPORT=%%i
+
+    if "%BCWEBPORT%" == "null" (
+       timeout /t 1
+       goto :bcwebport_loop_start
+    )   
+    if "%BCWEBPORT%" == "" (
+       timeout /t 1
+       goto :bcwebport_loop_start
+    )   
+    goto bcwebport_loop_exit
+goto :bcwebport_loop_start
+:bcwebport_loop_exit
+
+:grwebport_loop_start
+    echo polling the service grafana-grafana in namespace %NAMESPACE% to get the webport
+    kubectl get service --namespace=%NAMESPACE% grafana-grafana -o json | for /f %%i in ('jq .spec.ports[0].nodePort') do @echo %%i > %TMP%\GR_Webport.tmp
+    for /f %%i in (%TMP%\GR_Webport.tmp) do @set GRWEBPORT=%%i
+
+    if "%GRWEBPORT%" == "null" (
+       timeout /t 1
+       goto :grwebport_loop_start
+    )   
+    if "%GRWEBPORT%" == "" (
+       timeout /t 1
+       goto :grwebport_loop_start
+    )   
+    goto grwebport_loop_exit
+goto :grwebport_loop_start
+:grwebport_loop_exit
+
+:grafana_password
+kubectl get secret --namespace %NAMESPACE% grafana-grafana -o jsonpath="{.data.grafana-admin-password}" > %TMP%\GR_Passwrd.tmp
+certutil -f -decode %TMP%\GR_Passwrd.tmp %TMP%\GR_Passwrd.out
+for /f %%i in (%TMP%\GR_Passwrd.out) do @set GRPASS=%%i
+del %TMP%\GR_Passwrd.tmp
+del %TMP%\GR_Passwrd.out
 
 echo.
 echo Bluecompute was successfully installed!
 echo.
 echo To see Kubernetes Dashboard, paste the following in your terminal:
-echo set KUBECONFIG=%KUBECONFIG%
+echo  set KUBECONFIG=%KUBECONFIG%
 echo.
 echo Then run this command to connect to Kubernetes Dashboard:
-echo kubectl proxy
+echo  kubectl proxy
 echo.
 echo Then open a browser window and paste the following URL to see the Services created by Bluecompute:
-echo http://127.0.0.1:8001/api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard/#/service?namespace=default
+echo  http://127.0.0.1:8001/api/v1/proxy/namespaces/kube-system/services/kubernetes-dashboard/#/service?namespace=default
 echo.
 echo Finally, on another browser window, copy and paste the following URL for BlueCompute Web UI:
-echo http://%NODEIP%:%WEBPORT%
+echo  http://%NODEIP%:%BCWEBPORT%
+echo.
+echo To access the Grafana dashboards, copy and paste the following URL onto a browser window:
+echo  http://%NODEIP%:%GRWEBPORT%
+echo.
+echo The initial user is "admin" and the password is "%GRPASS%"
+echo To load more dashboards, execute the following script:
+echo  import_bc_grafana_dashboards.bat http://%NODEIP%:%GRWEBPORT% %GRPASS%
 
