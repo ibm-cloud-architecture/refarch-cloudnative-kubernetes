dc7a92c60aa2c4e00ca553a0be61a454583ca0f9
Gang Chen <gangchen@us.ibm.com>
2017-06-30 14:28:45 -0500

    Add windows clonePeer script
    Signed-off-by: Gang Chen <gangchen@us.ibm.com>

--- clonePeers.bat
+++ clonePeers.bat
@@ -5,22 +5,19 @@ set PATH=%PATH%;%CD%;%CD%\win_utils
 :setenvironment
 set git_org="ibm-cloud-architecture"
 set base_url="https://github.com/ibm-cloud-architecture"
-set repo_list=refarch-cloudnative-bluecompute-mobile ^
-refarch-cloudnative-bluecompute-web ^
-refarch-cloudnative-bluecompute-bff-ios ^
+set repo_list=refarch-cloudnative-bluecompute-web ^
 refarch-cloudnative-auth ^
 refarch-cloudnative-micro-inventory ^
 refarch-cloudnative-micro-orders ^
 refarch-cloudnative-micro-customer ^
-refarch-cloudnative-devops ^
-refarch-cloudnative-resiliency ^
-refarch-cloudnative-csmo
+refarch-cloudnative-devops-kubernetes ^
+refarch-cloudnative-kubernetes-csmo
 
 
-for %%repo in (%repo_list%) do (
-   echo cloning repository %%repo
-   set repo_url=%base_url%/%%repo
-   git clone %repo_url% -b kube-int --single-branch ../%%repo
+for %%r in (%repo_list%) do (
+   echo cloning repository %%r
+   set repo_url=%base_url%/%%r
+   git clone %repo_url% -b kube-int --single-branch /%%r
 )
 
 echo All github repository have cloned successfully!
