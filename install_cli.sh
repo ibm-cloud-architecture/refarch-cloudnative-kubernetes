a63c1dd8049409eab44f432fff91cd767252768a
Fabio Gomez <fabiogomez@us.ibm.com>
2017-07-10 16:41:01 -0500

    Fixed red hat install errors

--- install_cli.sh
+++ install_cli.sh
@@ -82,6 +82,10 @@ if [[ $? -ne 0 ]]; then
 	elif [[ $OSTYPE =~ .*linux.* ]]; then
 		# Linux
 		curl -o jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+
+		if [[ $? -ne 0 ]]; then
+			curl -L -o jq https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+		fi
 	fi
 
 	chmod +x ./jq
@@ -102,6 +106,10 @@ if [[ $? -ne 0 ]]; then
 	elif [[ $OSTYPE =~ .*linux.* ]]; then
 		# Linux
 		curl -o yaml https://github.com/mikefarah/yaml/releases/download/1.8/yaml_linux_amd64
+
+		if [[ $? -ne 0 ]]; then
+			curl -L -o yaml https://github.com/mikefarah/yaml/releases/download/1.8/yaml_linux_amd64
+		fi
 	fi
 
 	chmod +x ./yaml
