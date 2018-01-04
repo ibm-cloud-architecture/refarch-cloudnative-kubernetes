f9a67e953174ce9840189fc8d9778fcef2c751fc
jkwong888 <jkwong@ca.ibm.com>
2018-01-04 10:39:15 -0500

    add chart to PPA archive

--- ppa/build_ppa_archive.sh
+++ ppa/build_ppa_archive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CHART_VERSION=0.0.3
 
 _images="\
 ibmcase/bluecompute-dataloader:latest \
@@ -14,18 +15,32 @@ ibmcase/bluecompute-inventory:latest \
 ibmcase/bluecompute-orders:latest \
 ibmcase/bluecompute-web:latest"
 
-mkdir -p images
-mkdir -p charts
+mkdir ppa_archive
+mkdir -p ppa_archive/images
+mkdir -p ppa_archive/charts
 
 echo "Pulling images and saving in images/ ..."
 for i in ${_images}; do
   docker pull ${i}
   _shortname=`echo ${i} | cut -d'/' -f2 | cut -d':' -f1`
-  docker save ${i} -o images/${_shortname}.tar.gz
+  docker save ${i} -o ppa_archive/images/${_shortname}.tar.gz
 done
 
-echo "Copying chart from ../docs/charts/bluecompute-ce/bluecompute-ce-0.0.3.tgz to charts/ ..."
-cp ../docs/charts/bluecompute-ce/bluecompute-ce-0.0.3.tgz charts/
+if [ -f ./bluecompute-ce-${CHART_VERSION}.tgz ]; then
+  echo "Copying chart from ./bluecompute-ce-${CHART_VERSION}.tgz to charts/ ..."
+  cp ./bluecompute-ce-${CHART_VERSION}.tgz ppa_archive/charts/
+else
+  echo "Copying chart from ../docs/charts/bluecompute-ce-${CHART_VERSION}.tgz to charts/ ..."
+  cp ../docs/charts/bluecompute-ce/bluecompute-ce-${CHART_VERSION}.tgz ppa_archive/charts/
+fi
 
+echo "Updating manifest.json with chart version ..."
+sed -e 's/__CHART_VERSION__/'${CHART_VERSION}'/' manifest.json.tmpl > ppa_archive/manifest.json
 
-tar czvf bluecompute-ce-ppa-0.0.3.tgz images charts manifest.json manifest.yaml
+echo "Updating manifest.yaml with chart version ..."
+sed -e 's/__CHART_VERSION__/'${CHART_VERSION}'/' manifest.yaml.tmpl > ppa_archive/manifest.yaml
+
+echo "Building bluecompute-ce-ppa-${CHART_VERSION}.tgz ..."
+tar -C ./ppa_archive -czvf bluecompute-ce-ppa-${CHART_VERSION}.tgz images charts manifest.json manifest.yaml
+
+rm -rf ./ppa_archive
