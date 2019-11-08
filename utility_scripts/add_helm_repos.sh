cfc2ba2fed32f6384ebf71b00a5f3aca4fa5fbeb
Kevin Lau <689163+kevinwlau@users.noreply.github.com>
2019-11-08 14:12:53 -0600

    Update requirements yaml repositories. (#208)
    * Update requirements yaml repositories.
    
    * Create 0.1.0 release for umbrella chart
    
    * Update apis for kube 1.16, add helm release.
    
    * Update subchart release tars for k8s 1.16. Update requirements with k8s 1.16 compatible charts.
    
    * Remove 0.1.1 bluecompute release until finalized.

---
+++ utility_scripts/add_helm_repos.sh
@@ -0,0 +1,7 @@
+helm repo add bc-inventory https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-micro-inventory/microprofile/chart/release
+helm repo add bc-orders https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-micro-orders/microprofile/chart/release
+helm repo add bc-customer https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-micro-customer/microprofile/chart/release
+helm repo add bc-catalog https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-micro-catalog/microprofile/chart/release
+helm repo add bc-web https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-bluecompute-web/microprofile/chart/release
+helm repo add bc-auth https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-auth/microprofile/chart/release
+helm repo add services-bc-mp https://raw.githubusercontent.com/ibm-cloud-architecture/refarch-cloudnative-kubernetes/microprofile/docs/charts/services-bc-mp
